b116950aaa9116d489df2693cae41e3a06889d9e
Thomstrong <a80090@qq.com>
2019-03-23 12:08:31 +0800

    recover teacher and record

--- TianchiServer/classes/constants.py
+++ TianchiServer/classes/constants.py
@@ -1,10 +1,12 @@
 class Grade(object):
+    Zero = 0
     One = 1
     Two = 2
     Three = 3
 
 
 CLASS_GRADE_CHOICE = (
+    (Grade.Zero, '初三'),
     (Grade.One, '高一'),
     (Grade.Two, '高二'),
     (Grade.Three, '高三'),
---
+++ TianchiServer/scripts/recover_teacher_and_record.py
@@ -0,0 +1,79 @@
+import os
+
+from django.conf import settings
+from progress.bar import Bar
+
+# python manage.py runscript recover_teacher_and_record
+from classes.models import Class
+from courses.models import Course
+from teachers.models.teach_record import TeachRecord
+from teachers.models.teacher import Teacher
+from terms.models import Term
+
+
+def run():
+    grade_name_to_int = {
+        '初三': 0,
+        '高一': 1,
+        '高二': 2,
+        '高三': 3,
+    }
+    root = settings.BASE_DIR
+    file_name = '1_teacher'
+    file_path = os.path.join(root, 'scripts', 'data', file_name + '.csv')
+    err_file_path = os.path.join(root, 'scripts', 'data', file_name + '_err.csv')
+    err_record_file = open(err_file_path, 'w')
+    with open(file_path) as data_file:
+        data_file.readline()
+
+        lines = data_file.read().splitlines()
+        bar = Bar('Processing', max=len(lines))
+        for line in lines:
+            bar.next()
+            try:
+                split_line = line.replace('"', '').split(',')
+                term = split_line[0].split('-')
+                class_id = int(split_line[1])
+                class_name = split_line[2]
+                grade_name = split_line[3]
+
+                course_id = int(split_line[4])
+                course_name = split_line[5]
+
+                teacher_id = split_line[6]
+                teacher_name = split_line[7]
+
+                term_in_db, _ = Term.objects.get_or_create(
+                    start_year=int(term[0]),
+                    end_year=int(term[1]),
+                    order=int(term[2]),
+                )
+
+                class_in_db, _ = Class.objects.get_or_create(
+                    id=class_id,
+                    class_name=class_name,
+                    grade_name=grade_name_to_int[grade_name],
+                )
+
+                course_in_db, _ = Course.objects.get_or_create(
+                    id=course_id,
+                    name=course_name
+                )
+
+                teacher_in_db, _ = Teacher.objects.get_or_create(
+                    id=teacher_id,
+                    name=teacher_name
+                )
+
+                teach_record, _ = TeachRecord.objects.get_or_create(
+                    teacher=teacher_in_db,
+                    term=term_in_db,
+                    teach_class=class_in_db,
+                    course=course_in_db,
+                )
+            except Exception as e:
+                print(e)
+                err_record_file.write('{}\n'.format(line))
+                continue
+        bar.finish()
+    err_record_file.close()
