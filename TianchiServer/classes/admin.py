aad22493384a061437d124eaf7780b85580096d8
MeaKaka <3078858621@qq.com>
2019-04-01 16:19:22 +0800

    add campus attributes
    add campus attributes

--- TianchiServer/classes/admin.py
+++ TianchiServer/classes/admin.py
@@ -5,6 +5,6 @@ from classes.models import Class
 
 @admin.register(Class)
 class ClassAdmin(admin.ModelAdmin):
-    list_display = ('id', 'class_name', 'grade_name')
+    list_display = ('id', 'class_name', 'grade_name','campus_name')
     list_filter = ('grade_name',)
     search_fields = ('class_name', 'id')
--- TianchiServer/classes/models.py
+++ TianchiServer/classes/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
 from classes.constants import CLASS_GRADE_CHOICE
+from classes.constants import CLASS_CAMPUS_CHOICE
 
 
 class Class(models.Model):
     class_name = models.CharField(max_length=255, blank=False)
     grade_name = models.IntegerField(choices=CLASS_GRADE_CHOICE)
+    campus_name = models.IntegerField(choices=CLASS_CAMPUS_CHOICE,default=0)
 
     def __unicode__(self):
         return self.class_name
