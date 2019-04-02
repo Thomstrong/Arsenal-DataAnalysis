1c835baa53f0d0af83eeff0c2b7ad4cfa7882c1c
Thomstrong <a80090@qq.com>
2019-04-03 01:30:24 +0800

    add local settings

--- TianchiServer/TianchiServer/local_settings.py
+++ TianchiServer/TianchiServer/local_settings.py
@@ -1,1 +1,12 @@
 # add local settings will not effect others' code
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'tianchidb',
+#         'USER': 'postgres',
+#         'PASSWORD': '111111',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
--- TianchiServer/TianchiServer/settings.py
+++ TianchiServer/TianchiServer/settings.py
@@ -133,3 +133,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from .local_settings import *
+except Exception as e:
+    print('loading local settings failed:', repr(e))
+    pass
