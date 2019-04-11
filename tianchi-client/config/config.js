9736b548f636fec3d538747ece1e26953bec10c1
Thomstrong <a80090@qq.com>
2019-04-11 21:04:51 +0800

    init tianchi-client

---
+++ tianchi-client/config/config.js
@@ -0,0 +1,107 @@
+// https://umijs.org/config/
+import slash from 'slash2';
+import os from 'os';
+import pageRoutes from './router.config';
+import webpackPlugin from './plugin.config';
+import defaultSettings from '../src/defaultSettings';
+
+
+const { pwa, primaryColor } = defaultSettings;
+const { APP_TYPE, TEST } = process.env;
+const server = 'http://localhost:8000';
+const plugins = [
+  [
+    'umi-plugin-react',
+    {
+      antd: true,
+      dva: {
+        hmr: true,
+      },
+      locale: {
+        enable: true, // default false
+        default: 'zh-CN', // default zh-CN
+        baseNavigator: true, // default true, when it is true, will use `navigator.language` overwrite default
+      },
+      dynamicImport: {
+        loadingComponent: './components/PageLoading/index',
+        webpackChunkName: true,
+        level: 3,
+      },
+      pwa: pwa ?
+        {
+          workboxPluginMode: 'InjectManifest',
+          workboxOptions: {
+            importWorkboxFrom: 'local',
+          },
+        } : false,
+      ...(!TEST && os.platform() === 'darwin' ?
+        {
+          dll: {
+            include: ['dva', 'dva/router', 'dva/saga', 'dva/fetch'],
+            exclude: ['@babel/runtime', 'netlify-lambda'],
+          },
+          hardSource: false,
+        } : {}),
+    },
+  ],
+];
+
+export default {
+  // add for transfer to umi
+  plugins,
+  define: {
+    APP_TYPE: APP_TYPE || '',
+  },
+  treeShaking: true,
+  targets: {
+    ie: 11,
+  },
+  // 路由配置
+  routes: pageRoutes,
+  // Theme for antd
+  // https://ant.design/docs/react/customize-theme-cn
+  theme: {
+    'primary-color': primaryColor,
+  },
+  // proxy: {
+  //   '/api': {
+  //     target: `${server}/api`,
+  //     changeOrigin: true,
+  //     pathRewrite: { '^/api': '' },
+  //   },
+  // },
+  ignoreMomentLocale: true,
+  lessLoaderOptions: {
+    javascriptEnabled: true,
+  },
+  disableRedirectHoist: true,
+  cssLoaderOptions: {
+    modules: true,
+    getLocalIdent: (context, localIdentName, localName) => {
+      if (
+        context.resourcePath.includes('node_modules') ||
+        context.resourcePath.includes('ant.design.pro.less') ||
+        context.resourcePath.includes('global.less')
+      ) {
+        return localName;
+      }
+      const match = context.resourcePath.match(/src(.*)/);
+      if (match && match[1]) {
+        const antdProPath = match[1].replace('.less', '');
+        const arr = slash(antdProPath)
+          .split('/')
+          .map(a => a.replace(/([A-Z])/g, '-$1'))
+          .map(a => a.toLowerCase());
+
+        return `antd-pro${arr.join('-')}-${localName}`.replace(/--/g, '-');
+      }
+
+      return localName;
+    },
+  },
+  manifest: {
+    basePath: '/',
+  },
+
+  chainWebpack: webpackPlugin,
+};
---
+++ tianchi-client/src/pages/.umi/LocaleWrapper.jsx
@@ -0,0 +1,92 @@
+
+import { _setIntlObject, addLocaleData, IntlProvider, intlShape } from 'umi-plugin-locale';
+
+const InjectedWrapper = (() => {
+  let sfc = (props, context) => {
+    _setIntlObject(context.intl);
+    return props.children;
+  };
+  sfc.contextTypes = {
+    intl: intlShape,
+  };
+  return sfc;
+})();
+
+import 'moment/locale/pt-br';
+import 'moment/locale/zh-cn';
+import 'moment/locale/zh-tw';
+
+const baseNavigator = true;
+const useLocalStorage = true;
+
+import { LocaleProvider } from 'antd';
+import moment from 'moment';
+import 'moment/locale/zh-cn';
+let defaultAntd = require('antd/lib/locale-provider/zh_CN');
+defaultAntd = defaultAntd.default || defaultAntd;
+
+const localeInfo = {
+  'en-US': {
+    messages: {
+      ...require('/Users/caiwenqiang/Documents/Tianchi-DataView/Arsenal-DataAnalysis/tianchi-client/src/locales/en-US.js').default,
+    },
+    locale: 'en-US',
+    antd: require('antd/lib/locale-provider/en_US'),
+    data: require('react-intl/locale-data/en'),
+    momentLocale: '',
+  },
+  'pt-BR': {
+    messages: {
+      ...require('/Users/caiwenqiang/Documents/Tianchi-DataView/Arsenal-DataAnalysis/tianchi-client/src/locales/pt-BR.js').default,
+    },
+    locale: 'pt-BR',
+    antd: require('antd/lib/locale-provider/pt_BR'),
+    data: require('react-intl/locale-data/pt'),
+    momentLocale: 'pt-br',
+  },
+  'zh-CN': {
+    messages: {
+      ...require('/Users/caiwenqiang/Documents/Tianchi-DataView/Arsenal-DataAnalysis/tianchi-client/src/locales/zh-CN.js').default,
+    },
+    locale: 'zh-CN',
+    antd: require('antd/lib/locale-provider/zh_CN'),
+    data: require('react-intl/locale-data/zh'),
+    momentLocale: 'zh-cn',
+  },
+  'zh-TW': {
+    messages: {
+      ...require('/Users/caiwenqiang/Documents/Tianchi-DataView/Arsenal-DataAnalysis/tianchi-client/src/locales/zh-TW.js').default,
+    },
+    locale: 'zh-TW',
+    antd: require('antd/lib/locale-provider/zh_TW'),
+    data: require('react-intl/locale-data/zh'),
+    momentLocale: 'zh-tw',
+  },
+};
+
+let appLocale = {
+  locale: 'zh-CN',
+  messages: {},
+  data: require('react-intl/locale-data/zh'),
+  momentLocale: 'zh-cn',
+};
+if (useLocalStorage && localStorage.getItem('umi_locale') && localeInfo[localStorage.getItem('umi_locale')]) {
+  appLocale = localeInfo[localStorage.getItem('umi_locale')];
+} else if (localeInfo[navigator.language] && baseNavigator){
+  appLocale = localeInfo[navigator.language];
+} else {
+  appLocale = localeInfo['zh-CN'] || appLocale;
+}
+window.g_lang = appLocale.locale;
+appLocale.data && addLocaleData(appLocale.data);
+
+export default function LocaleWrapper(props) {
+  let ret = props.children;
+  ret = (<IntlProvider locale={appLocale.locale} messages={appLocale.messages}>
+    <InjectedWrapper>{ret}</InjectedWrapper>
+  </IntlProvider>)
+  ret = (<LocaleProvider locale={appLocale.antd ? (appLocale.antd.default || appLocale.antd) : defaultAntd}>
+    {ret}
+  </LocaleProvider>);
+  return ret;
+}
