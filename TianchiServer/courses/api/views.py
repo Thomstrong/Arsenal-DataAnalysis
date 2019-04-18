e58456214cafd7989117b6891f5d740781ddbdd9
Thomstrong <a80090@qq.com>
2019-04-19 01:48:39 +0800

    选课分布及玉珏图对接完成

--- TianchiServer/courses/api/views.py
+++ TianchiServer/courses/api/views.py
@@ -6,26 +6,47 @@ from rest_framework.response import Response
 
 from courses.api.serializers import CourseRecordSerializer
 from courses.models.course_record import CourseRecord
-from utils.decorators import performance_analysis
+from utils.decorators import required_params
 
 
 class CourseRecordViewSet(viewsets.ModelViewSet):
     queryset = CourseRecord.objects.all()
     serializer_class = CourseRecordSerializer
 
-    @performance_analysis(True)
+    @required_params(params=['type'])
     @list_route(
         methods=['GET'],
     )
     def distribution(self, request):
-        records = CourseRecord.objects.filter(
-            year__gt=2016
-        ).values(
-            'course__name',
-            'year',
-            'student__sex',
-        ).annotate(
-            count=Count('student_id', distinct=True)
-        )
+        type = request.query_params.get('type', '')
+        if not type:
+            return Response('type 输入有误！', status=400)
+        if type == 'selection':
+            records = CourseRecord.objects.filter(
+                year__gt=2016
+            ).values(
+                'course_id',
+                'year',
+                'student__sex',
+            ).annotate(
+                count=Count('student_id', distinct=True)
+            )
 
-        return Response(records)
+            return Response(records)
+
+        if type == 'course_percent':
+            year = request.query_params.get('year', 2019)
+            total = CourseRecord.objects.filter(year=year).count()
+
+            records = CourseRecord.objects.filter(
+                year=year
+            ).values(
+                'course_id',
+            ).annotate(
+                count=Count('student_id', distinct=True)
+            ).order_by('count')
+
+            return Response({
+                'total': total,
+                'records': records
+            })
