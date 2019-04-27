8cdb37d42833e2bb690d2eccf64a3e07bcf808c1
hxh <hxh@hxhdeMacBook-Pro.local>
2019-04-28 00:57:50 +0800

    单个学生消费情况的页面，上两个图表的文字处理

--- tianchi-client/src/pages/Account/Center/ConsumptionOverallLineChart.js
+++ tianchi-client/src/pages/Account/Center/ConsumptionOverallLineChart.js
@@ -1,27 +1,52 @@
 /**
  * Created by 胡晓慧 on 2019/4/13.
  */
-import React, { memo } from "react";
-import { Card, Col, Empty, Row } from 'antd';
-import { Axis, Chart, Geom, Legend, Tooltip } from "bizcharts";
-import { OneTimelineChart } from '@/components/Charts';
+import React, {memo} from "react";
+import {Card, Col, Empty, Row} from 'antd';
+import {Axis, Chart, Geom, Legend, Tooltip} from "bizcharts";
+import {OneTimelineChart} from '@/components/Charts';
+import {Typography, Divider} from 'antd';
+
+const { Paragraph, Text} = Typography;
 
 let chartIns = null;
 
+//todo 由后端传入
+const money = 2000;
+const precent = "20%";
+const cishu = 7;
+const time = "8时";
+const timemoney = 20.6;
+
 const ConsumptionOverallLineChart = memo(
-  ({ hourlyAvgCost, dailySumCost, maxHourlyAvg }) => {
+  ({hourlyAvgCost, dailySumCost, maxHourlyAvg}) => {
     if (chartIns) {
       const geoms = chartIns.getAllGeoms();
       for (let geom of geoms) {
         geom && geom.show();
       }
     }
+    let timeCount =0;
+    let maxTime = 0;
+    let maxMoney = 0;
+    for (let i = 0; i < hourlyAvgCost.length; i++) {
+       if (hourlyAvgCost[i].avg_cost !== 0){
+         timeCount =timeCount+1;
+         if (hourlyAvgCost[i].avg_cost>=maxMoney) {
+           maxMoney = hourlyAvgCost[i].avg_cost;
+           maxTime = i
+         }
+       }
+    }
+
+
+    console.log(hourlyAvgCost);
     return (
-      <Card title="总体消费情况一览" bordered={false} style={{ width: '100%' }}>
-        <Row>
-          <Col span={20}>
-            <Card title="总体消费趋势" bordered={false} style={{ width: '100%' }} hoverable={true}>
-              {dailySumCost.length ? <OneTimelineChart
+      <Card title="总体消费情况一览" bordered={false} style={{width: '100%'}}>
+        <Card title="总体消费趋势" bordered={false} style={{width: '100%'}} hoverable={true}>
+          {dailySumCost.length ? <Row>
+            <Col span={20}>
+              <OneTimelineChart
                 height={300}
                 data={dailySumCost.map((data) => {
                   return {
@@ -29,26 +54,27 @@ const ConsumptionOverallLineChart = memo(
                     y: data.total
                   };
                 })}
-              /> : <Empty/>}
-            </Card>
-          </Col>
-          <Col span={3} offset={1}>
-            <Card title="总结" bordered={false} style={{ width: '100%' }} hoverable={true}>
-              <p>该同学消费趋势比较稳定</p>
-            </Card>
-          </Col>
-        </Row>
-        <Row>
-          <Col span={3}>
-            <Card title="总结" bordered={false} style={{ width: '100%' }} hoverable={true}>
-              <p>该同学消费水平属于本校消费的平均水平</p>
-            </Card>
-          </Col>
-          <Col span={20} offset={1}>
-            <Card title="不同时间点平均消费对比" bordered={false} style={{ width: '100%' }} hoverable={true}>
-              {dailySumCost.length ? <Chart
-                height={400}
+              />
+            </Col>
+            <Col span={3} offset={1}>
+              <Paragraph>该学生平均日消费为<Text strong style={{color: "#cc4756"}}>¥{money}</Text>元</Paragraph>
+              <Paragraph>超过<Text strong style={{color: "#cc4756"}}>{precent}</Text>的学生</Paragraph>
+            </Col>
+          </Row> : <Empty/>}
+        </Card>
+        <Card title="不同时间点平均消费对比" bordered={false} style={{width: '100%'}} hoverable={true}>
+          {dailySumCost.length ? <Row>
+            <Col span={3}>
+              <Paragraph>共有<Text strong style={{color: "#cc4756"}}>{timeCount}</Text>个时间段产生过消费;</Paragraph>
+              <Paragraph>其中,平均消费最高的金额出现在<Text strong style={{color: "#cc4756"}}>{maxTime}时</Text>,平均消费为<Text strong
+                                                                                                         style={{color: "#cc4756"}}>¥{maxMoney}</Text></Paragraph>
+            </Col>
+            <Col span={20} offset={1}>
+
+              <Chart
+                height={300}
                 data={hourlyAvgCost}
+                padding={[10, "auto", 20, "auto"]}
                 scale={{
                   hour: {
                     min: 0,
@@ -95,8 +121,8 @@ const ConsumptionOverallLineChart = memo(
                       }
                     }
                   ]}
-                  onClick={({ item, checked }) => {
-                    const { value } = item;
+                  onClick={({item, checked}) => {
+                    const {value} = item;
                     const geoms = chartIns.getAllGeoms();
                     for (let geom of geoms) {
                       if (geom.getYScale().alias === value) {
@@ -110,15 +136,15 @@ const ConsumptionOverallLineChart = memo(
                   }}
                 />
                 <Axis name="hour"/>
-                {/*<Axis name="total_avg" visible={false}/>*/}
+                <Axis name="total_avg" visible={false}/>
                 <Tooltip/>
                 <Geom type="interval" position="hour*avg_cost" color="#0099CC"/>
                 <Geom type="line" position="hour*total_avg" color="#FF9900" size={2} shape="smooth"/>
                 <Geom type="point" position="hour*total_avg" color="#FF9900" size={3} shape="circle"/>
-              </Chart> : <Empty/>}
-            </Card>
-          </Col>
-        </Row>
+              </Chart>
+            </Col>
+          </Row> : <Empty/>}
+        </Card>
       </Card>
     );
   }
