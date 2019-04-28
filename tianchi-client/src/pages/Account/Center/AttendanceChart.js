5ba5721cdb3013243f86b8f0e66756966b24cfea
hxh <hxh@hxhdeMacBook-Pro.local>
2019-04-28 15:16:14 +0800

    学生考勤页面的文字分析的处理

--- tianchi-client/src/pages/Account/Center/AttendanceChart.js
+++ tianchi-client/src/pages/Account/Center/AttendanceChart.js
@@ -4,12 +4,13 @@
 import React, { memo } from "react";
 import { Card, Col, Empty, Row } from 'antd';
 import { Axis, Chart, Geom, Legend, Tooltip } from "bizcharts";
-
+import { Typography } from 'antd';
+const { Paragraph, Text } = Typography;
 
 const AttendanceChart = memo(
   ({ kaoqinData, kaoqinSummary, termList, loading }) => (
     <Card loading={loading} title="考勤记录" bordered={false} style={{ width: '100%' }}>
-      {termList.length ? <Row>
+      {termList.length ? <Row type="flex" align="middle">
         <Col span={20}>
           <Chart
             scale={{
@@ -35,13 +36,13 @@ const AttendanceChart = memo(
           </Chart>
         </Col>
         <Col span={4}>
-          <p>在考勤记录信息中,该学生共
+          <Paragraph>在考勤记录信息中,该学生共<Text strong style={{color:"#c6464a"}}>
             {kaoqinSummary.map(data => {
               if (data.name === '进校') {
                 return;
               }
               return `${data.name} ${data.count} 次, `;
-            })} </p>
+            })} </Text></Paragraph>
         </Col>
       </Row> : <Empty description='该同学暂无不良考勤数据'/>}
     </Card>
