906577badb78ae21db42038b603fff6233d1ff2c
Thomstrong <a80090@qq.com>
2019-05-07 00:09:30 +0800

    update deploy script

--- tianchi-client/deploy.sh
+++ tianchi-client/deploy.sh
@@ -3,7 +3,8 @@ cd /root/Tianchi-Analysis/Arsenal-DataAnalysis/tianchi-client &&
 git pull origin master &&
 yarn install &&
 yarn build &&
-yarn docker-hub:build &&
 docker stop tianchi-client &&
 docker rm tianchi-client &&
+docker image rm tianchi-client &&
+yarn docker-hub:build &&
 docker run -d -p 80:80 --name tianchi-client tianchi-client
