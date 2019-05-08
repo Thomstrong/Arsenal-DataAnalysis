d0e558b4974916ea86a4cca0a5e0a9a83c4464af
Thomstrong <a80090@qq.com>
2019-05-09 02:03:55 +0800

    优化部署脚本

--- tianchi-client/hotDeploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd /root/Tianchi-Analysis/Arsenal-DataAnalysis/tianchi-client &&
-git pull origin master &&
-yarn install &&
-yarn build &&
-yarn docker-hub:build &&
-docker stop tianchi-client &&
-docker rm tianchi-client &&
-docker run -d -p 80:80 --name tianchi-client tianchi-client
