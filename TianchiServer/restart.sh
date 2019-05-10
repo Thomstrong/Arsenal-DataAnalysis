8028b33b860158ec2f50a5ef67be63a11ccc514d
Thomstrong <a80090@qq.com>
2019-05-10 11:42:29 +0800

    refine restart

--- TianchiServer/restart.sh
+++ TianchiServer/restart.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+kill -9 `lsof -l | grep 'TCP\ \*:8000' | tail -n 1 | awk '{print $2}'` &&
 cd /root/Tianchi-Analysis/Arsenal-DataAnalysis/TianchiServer &&
 source env/bin/activate &&
 pip install -i https://pypi.tuna.tsinghua.edu.cn/simple/ -r requirements.txt &&
